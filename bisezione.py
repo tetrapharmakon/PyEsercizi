ce54f45c7fd9c852dc74eed166f5bdad17acbe0f
Indrjo Dedej <dedej.indrjo@gmail.com>
2019-10-13 17:42:06 +0200

    latest revision: 17:42 10/13/2019

--- bisezione.py
+++ bisezione.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python3
 
-# media di sue numeri
-def m(a,b):
-	return (a+b)/2
-
 # definizione della funzione di ricerca per bisezione
 def CercaRadice(x,n,a,b,precisione):
 	# scegliere a < b !
+	if a > b:
+		return "deve essere a < b"
 	# se uno degli estremi è la radice, restituisci quello
 	if a**n == x:
 		return a
 	if b**n == x:
 		return b
+	# calcola la media di a e b
+	m = (a+b)/2
 	# se la media è la radice, restituisci quella
-	if m(a,b)**n == x:
-		return m(a,b)
+	if m**n == x:
+		return m
 	# se la differenza tra b e a è inferiore ad una soglia, restituisci la loro media
 	if b-a < precisione:
-		return m(a,b)
+		return m
 	# altrimenti, la funzione richiama se stessa
-	if a**n < x and m(a,b)**n > x:
-		return CercaRadice(x,n,a,m(a,b),precisione)
-	if m(a,b)**n < x and b**n > x:
-		return CercaRadice(x,n,m(a,b),b,precisione)
+	if a**n < x and m**n > x:
+		return CercaRadice(x,n,a,m,precisione)
+	if m**n < x and b**n > x:
+		return CercaRadice(x,n,m,b,precisione)
 
-#print(CercaRadice(3,3,1,3,10**-4))
+print(CercaRadice(3,3,1,3,10**-4))
 
--- moneta50.py
+++ moneta50.py
@@ -1,17 +1,19 @@
+
 def valoremonete(a,b,c,d):
     return (a+2*b+5*c+10*d)
+
 def cambio(a,b,c,d,x):
-    if valoremonete(a,b,c,d)==50:
-        x=x+1
+	if valoremonete(a,b,c,d)==50:
+		x=x+1
     if d==5:
-        print (x)
-    elif valoremonete(0,0,c,d)>=50:
-        cambio(0,0,0,d+1,x)
-    elif valoremonete(0,b,c,d)>=50:
-        cambio(0,0,c+1,d,x)
+		print(x)
+	elif valoremonete(0,0,c,d)>=50:
+		cambio(0,0,0,d+1,x)
+	elif valoremonete(0,b,c,d)>=50:
+		cambio(0,0,c+1,d,x)
     elif valoremonete(a,b,c,d)>=50:
-        cambio(0,b+1,c,d,x)
-    else:
-        cambio(50-(2*b+5*c+10*d),b,c,d,x)
-        
+		cambio(0,b+1,c,d,x)
+	else:
+		cambio(50-(2*b+5*c+10*d),b,c,d,x)
+
 cambio(0,0,0,0,0)
