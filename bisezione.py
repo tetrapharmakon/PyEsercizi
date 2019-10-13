2bc228aa09333232a57c2ae8f702fb0d20bfaf9a
Indrjo Dedej <46417157+IndrjoDedej@users.noreply.github.com>
2019-10-13 08:21:18 +0200

    Add files via upload

--- bisezione.py
+++ bisezione.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python3
 
+# media di sue numeri
 def m(a,b):
 	return (a+b)/2
 
-def bisezione(a,b):
-	if abs(a-b) < 0.001:
+# definizione della funzione di ricerca per bisezione
+def CercaRadice(x,n,a,b,precisione):
+	# scegliere a < b !
+	# se uno degli estremi è la radice, restituisci quello
+	if a**n == x:
+		return a
+	if b**n == x:
+		return b
+	# se la media è la radice, restituisci quella
+	if m(a,b)**n == x:
 		return m(a,b)
-	if a*a<2 and m(a,b)*m(a,b)>2:
-		return bisezione(a,m(a,b))
-	else:
-		return bisezione(m(a,b),b)
+	# se la differenza tra b e a è inferiore ad una soglia, restituisci la loro media
+	if b-a < precisione:
+		return m(a,b)
+	# altrimenti, la funzione richiama se stessa
+	if a**n < x and m(a,b)**n > x:
+		return CercaRadice(x,n,a,m(a,b),precisione)
+	if m(a,b)**n < x and b**n > x:
+		return CercaRadice(x,n,m(a,b),b,precisione)
 
-print(bisezione(1,2))
+#print(CercaRadice(3,3,1,3,10**-4))
+
