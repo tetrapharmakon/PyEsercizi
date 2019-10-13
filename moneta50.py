3b68e445d0652cef306aab20c1fb5ae0e90b1d47
Carbonara00 <56485537+Carbonara00@users.noreply.github.com>
2019-10-13 11:29:58 +0200

    Update moneta50.py

--- moneta50.py
+++ moneta50.py
@@ -1,17 +1,17 @@
 def valoremonete(a,b,c,d):
     return (a+2*b+5*c+10*d)
 def cambio(a,b,c,d,x):
-    if valoremonete(a,b,c,d)==50:
+    if valoremonete(a,b,c,d)==20:
         x=x+1
-    if d==5:
+    if d==2:
         print (x)
-    elif valoremonete(0,0,c,d)>=50:
+    elif valoremonete(0,0,c,d)>=20:
         cambio(0,0,0,d+1,x)
-    elif valoremonete(0,b,c,d)>=50:
+    elif valoremonete(0,b,c,d)>=20:
         cambio(0,0,c+1,d,x)
-    elif valoremonete(a,b,c,d)>=50:
+    elif valoremonete(a,b,c,d)>=20:
         cambio(0,b+1,c,d,x)
     else:
-        cambio(50-(2*b+5*c+10*d),b,c,d,x)
+        cambio(20-(2*b+5*c+10*d),b,c,d,x)
         
 cambio(0,0,0,0,0)
